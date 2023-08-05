d623ac3159ba9942a0b95403c8f6ffc5ba738ff2
Serhiy Chepa <sergei.chepa@gmail.com>
2023-08-05 09:54:01 +0200

    [update] 1st task of the homework is complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const container = document.querySelector(".gallery");
-console.log(container);
-
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
-
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
-// TODO: Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
 
 function createMarkup(arr) {
   return arr
@@ -43,12 +28,31 @@ container.addEventListener("click", clickHandler);
 
 function clickHandler(evt) {
   evt.preventDefault();
+
+  document.addEventListener("keydown", escapeHandler);
+
   if (evt.target === evt.currentTarget) {
     return;
   }
 
   const sourceImg = evt.target.dataset.source;
-  const instance = basicLightbox
-    .create(`<img width="1400" height="900" src="${sourceImg}">`)
-    .show();
+
+  // creating popup
+  const instance = basicLightbox.create(
+    `<img width="1400" height="900" src="${sourceImg}">`,
+    // removing keyboard event listener after popup closed
+    {
+      onClose: (instance) => {
+        document.removeEventListener("keydown", escapeHandler);
+      },
+    }
+  );
+  instance.show();
+
+  // closing popup with escape key
+  function escapeHandler(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
 }
