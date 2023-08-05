44dffb7f5d20fa54f8dab0784c15b7cfe87c2d92
Serhiy Chepa <sergei.chepa@gmail.com>
2023-08-05 10:30:10 +0200

    [update] 2nd task ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img 
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}" />
+          </a>
+        </li>
+        `
+    )
+    .join("");
+}
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+container.addEventListener("click", clickHandler);
+
+function clickHandler(evt) {
+  evt.preventDefault();
+
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
